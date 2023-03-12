e19bd262ae356ed5fda8b986ba1d9dea48bc4d9b
Paul Kim Set B <pkim59@my.bcit.ca>
2023-03-12 15:24:40 -0700

    Array defined, now writing conditionals

---
+++ js/lab7.js
@@ -0,0 +1,16 @@
+const courseList = {
+    course1 : {code: "ACIT 1630", name: "Database Systems"},
+    course2 : {code: "ORGB 1100", name: "Organizational Behaviour"},
+    course3 : {code: "COMM 1116", name: "Business Communications"},
+}
+
+let method = false
+while (method === false) {
+    let ask = prompt("Do you know what my course numbers are? Enter them here and find out!")
+        if (ask === NaN) {
+            alert("Yeah, that's not really a number...")
+        else if (ask.length) {
+            
+        }
+}
+}
