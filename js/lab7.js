01d6ad3ecc77f3b53b65993539b75eb4a94a8803
Paul Kim Set B <pkim59@my.bcit.ca>
2023-03-12 17:38:34 -0700

    READ THE LAB MORE CAREFULLY! Fixing IF statement.

--- js/lab7.js
+++ js/lab7.js
@@ -1,36 +1,46 @@
-const courses = {
-    course1 : {code: "ACIT 1630", name: "Database Systems"},
-    course2 : {code: "ORGB 1100", name: "Organizational Behaviour"},
-    course3 : {code: "COMM 1116", name: "Business Communications"},
-}
+const courses = [
+    {code: "ACIT 1630", name: "Database Systems"},
+    {code: "ORGB 1100", name: "Organizational Behaviour"},
+    {code: "COMM 1116", name: "Business Communications"},
+];
 
-let method = false
+let test = false;
 
-while (method === false) {
-    let ask = prompt("Do you know what my course numbers are? Enter them here and find out!")
-        if (ask == NaN) {
-            alert("Yeah, that's not really a number...")
-        }
-        else if (ask.length !== 4) {
-                alert("Please enter 4 digits/numbers! No more, no less.")
-        } else {
-            alert("you entered: " + ask);
-            let method = true
-            break
-        }
-    
+let ask = prompt("Do you know what my course numbers are? Enter them here and find out!")
+console.log(ask)
+while (test === false) {
+    if (isNaN(ask)) {
+        alert("Yeah, that's not really a number...");
+    }
+    else if (ask.length !== 4) {
+        alert("Please enter 4 digits/numbers! No more, no less.");
+    } else {
+        alert("you entered: " + ask);
+        test = true;
     }
 
+    }
+
+let method = true;
+
 while (method === true) {
+    let correct = false;
+
     for (let coursenum = 0; coursenum < courses.length; coursenum++) {
-        if (courses[courseID][code].includes(ask)) {
-            let coursedetails = courses[courseID][code].includes(ask)
-            console.log(`Yes, I am currently taking the course ${coursedetails.code} - ${coursedetails.name}`)
-            method = false
+        if (courses[coursenum].code.includes(ask)) {
+            console.log(`Yes, I am currently taking ${courses[coursenum].code} - ${courses[coursenum].name}`);
+            correct = true;
+            method = false;
+            break
         }
-        else {
-            courses.push({ask : {code: ask, name: null}})
-            console.log(`succeeded in entering ${ask} as a new course!`)
-        }
-    }   
+
+if (correct === false) {
+    const newcourse = {code: ask, name: null};
+    courses.push(newcourse);
+    console.log(`succeeded in entering ${ask} as a new course!`);
+    alert("new course added!");
+    correct = true;
+    method = false;
+    }
+    }
 }
